d3e18df718b0fa5944e75119764216a56d2f80ee
muthulingam <muthulingam.a@gmail.com>
2024-06-10 11:03:08 +0530

    created xcode project for Diagnal Task
    created xcode project for Diagnal Task

---
+++ DiagnalApp/DiagnalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiagnalApp
+//
+//  Created by Muthulingam on 10/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
